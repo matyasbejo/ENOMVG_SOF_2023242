a8c5004098bb38f923533aadd21d3e1326e25e0f
matyasbejo <bejomatyi@gmail.com>
2024-06-20 21:06:32 +0200

    first controller and views added, program.cs adjusted accordingly

---
+++ ENOMVG_SOF_2023242/Controllers/SchoolController.cs
@@ -0,0 +1,35 @@
+﻿using ENOMVG_SOF_2023242.Logic;
+using ENOMVG_SOF_2023242.Models;
+using ENOMVG_SOF_2023242.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ENOMVG_SOF_2023242.Controllers
+{
+    public class SchoolController : Controller
+    {
+        ISchoolRepository repo;
+
+        public SchoolController(ISchoolRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repo.ReadAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]  
+        public IActionResult Create(School school)
+        {
+            repo.Create(school);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- ENOMVG_SOF_2023242/Program.cs
+++ ENOMVG_SOF_2023242/Program.cs
@@ -1,6 +1,21 @@
+using ENOMVG_SOF_2023242.Data;
+using ENOMVG_SOF_2023242.Logic;
+using ENOMVG_SOF_2023242.Models;
+using ENOMVG_SOF_2023242.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ISchoolRepository, SchoolRepository>();
+builder.Services.AddTransient<IStudentRepository, StudentRepository>();
+builder.Services.AddTransient<ITeacherRepository, TeacherRepository>();
+builder.Services.AddDbContext<SchollingDbContext>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=School}/{action=Index}/{id?}");
 
 app.Run();
