a7e8c4ef8b9abf6e6b44fdf6b38c1f43b1b9d7da
matyasbejo <bejomatyi@gmail.com>
2024-06-20 21:05:40 +0200

    interface reworked

--- ENOMVG_SOF_2023242/Repository/ISchoolRepository.cs
+++ ENOMVG_SOF_2023242/Repository/ISchoolRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace ENOMVG_SOF_2023242.Repository
+﻿using ENOMVG_SOF_2023242.Models;
+
+namespace ENOMVG_SOF_2023242.Repository
 {
     public interface ISchoolRepository
     {
+        void Create(School _item);
+        void Delete(int _id);
+        School Read(int _id);
+        IQueryable<School> ReadAll();
+        void Update(School _item);
     }
 }
